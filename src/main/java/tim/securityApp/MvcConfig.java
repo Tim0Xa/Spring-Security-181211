a8a5231deead1753c3157824a63171b5393cfbe8
Tim0Xa <kogtim@gmial.com>
2018-12-11 12:46:16 +0200

    Commit IT

---
+++ src/main/java/tim/securityApp/MvcConfig.java
@@ -0,0 +1,17 @@
+package tim.securityApp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewContollers(ViewControllerRegistry vc){
+        vc.addViewController("/home").setViewName("home");
+        vc.addViewController("/").setViewName("home");
+        vc.addViewController("/hello").setViewName("hello");
+        vc.addViewController("/login").setViewName("");
+
+    }
+}
