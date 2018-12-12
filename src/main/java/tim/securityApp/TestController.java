377fc23add3c33e961035156c59fa272ad1e4ae4
Tim0Xa <kogtim@gmial.com>
2018-12-12 13:33:34 +0200

    Manualy, MVC controller added. Issue: doesn't wire pages by automaticly by addViewController

---
+++ src/main/java/tim/securityApp/TestController.java
@@ -0,0 +1,13 @@
+package tim.securityApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping({"/", "/home"})
+    public String homePage(){
+        return "home";
+    }
+}
